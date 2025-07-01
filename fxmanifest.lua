4b2e47d69e084276cfcac9badf574bc70741b70b
ScriptureScripts <arkennetworks@gmail.com>
2025-07-01 14:29:03 +1000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Scripture'
+description 'Gang Manager Script'
+version '1.0.0'
+
+server_scripts {
+    'config.lua',
+    'server/main.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/app.js'
+}
