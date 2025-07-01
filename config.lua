9dce6e94467fa529578b89793e6c202c8d34e6ee
ScriptureScripts <arkennetworks@gmail.com>
2025-07-01 14:28:50 +1000

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+Config.AllowedAce = 'God'
+Config.Webhook = 'https://discord.com/api/webhooks/1389448803459006584/v_PRD1j_6OaS4EBSWlASbcVvshlmLhJRNEdjndXd9lLxpcjUg-yUD2vh5Y6LpGOX8C-P'
+Config.Ranks = {
+    'Leader',
+    'Hierarchy',
+    'PROBIE',
+    'TRIAL WHITELIST',
+    'WHITELISTED',
+    'CREW',
+    'ORG'
+}
